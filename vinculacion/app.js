ac18693fa83b43ec26fca74ff054d7c253301d05
Fredy Roman <johnfredyromanagudelo@gmail.com>
2022-07-22 14:56:04 -0500

    se adicionaron mas lineas

--- vinculacion/app.js
+++ vinculacion/app.js
@@ -1,2 +1,9 @@
 console.log('hola fredy');
 console.log('hola fredy2');
+
+
+function AlumnoUniversitario(nombre, edad){ 
+    this.nombre = nombre 
+    this.edad = edad 
+    this.numMatricula = null 
+}
